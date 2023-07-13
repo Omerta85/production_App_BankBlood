9aab92e98796aadc6712022a5d300e46bf8cd943
Omerta85 <104519261+Omerta85@users.noreply.github.com>
2023-07-13 16:14:34 +0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () =>{
     try{
-        await mongoose.connect(process.env.MONGO_URL);
+        await  mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });;
         console.log(`Connected To Mongodb Database${mongoose.connection.host}`.bgMagenta.blue);
     } catch (error) {
         console.log(`Mongodb Database ${error}`.bgRed.white)
