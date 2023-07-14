f7f6cd9d13a7bd02217412b29c04765b06e61acc
Omerta85 <666Omerta@gmail.com>
2023-07-14 18:24:25 +0300

    fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-
+mongoose.set('strictQuery', false);
 const connectDB = async () =>{
     try{
-        await  mongoose.connect(process.env.MONGO_URLe);
+        await  mongoose.connect(process.env.MONGO_URL);
         console.log(`Connected To Mongodb Database${mongoose.connection.host}`);
     } catch (error) {
         console.log(`Mongodb Database ${error}`)
