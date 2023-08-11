20e77c7aaac3628cde9d75ef0f0388c8179241b7
Omerta85 <666Omerta@gmail.com>
2023-08-11 20:18:14 +0300

    fixed frontend

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,15 +1,90 @@
-import { getCurrentUser, userLogin, userRegister } from "../redux/features/auth/authActions";
-import { store } from "../redux/store";
-import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
+// import { getCurrentUser, userLogin, userRegister } from "../redux/features/auth/authActions";
+// import { store } from "../redux/store";
+// import { toast } from "react-toastify";
+// import { useNavigate } from "react-router-dom";
+//
+// export const handleLogin = (e, email, password, role) => {
+//     e.preventDefault();
+//     try {
+//         if (!role || !email || !password) {
+//             toast.error("Please Provide All Fields");
+//         }
+//         store.dispatch(userLogin({ email, password }));
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+//
+// export const handleRegister = async (
+//     e,
+//     name,
+//     role,
+//     email,
+//     password,
+//     phone,
+//     organisationName,
+//     address,
+//     hospitalName,
+//     website
+// ) => {
+//     e.preventDefault();
+//     try {
+//         if (!role || !email || !password || !phone || !address || !website || (!name && !organisationName && !hospitalName)) {
+//             toast.error("Please Provide All Fields");
+//         }
+//
+//         await store.dispatch(getCurrentUser());
+//
+//         const state = store.getState();
+//         const users = state.auth.user;
+//
+//         if (users === null) {
+//             toast.error("User data is still loading. Please try again.");
+//             return;
+//         }
+//
+//         const existingUser = users.find((user) => user.email === email);
+//         if (existingUser) {
+//             toast.error("This email is already registered");
+//             return;
+//         }
+//
+//         await store.dispatch(
+//             userRegister({
+//                 name,
+//                 role,
+//                 email,
+//                 password,
+//                 phone,
+//                 organisationName,
+//                 address,
+//                 hospitalName,
+//                 website,
+//             })
+//         );
+//
+//         toast.success("Registration successful!");
+//
+//         const navigate = useNavigate();
+//         navigate("/login");
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
 
-export const handleLogin = (e, email, password, role) => {
+
+import { userLogin, userRegister } from "../redux/features/auth/authActions";
+import {store} from "../redux/store";
+import {toast} from "react-toastify";
+import {useNavigate} from "react-router-dom";
+
+export const handleLogin = async (e, email, password, role) => {
     e.preventDefault();
     try {
         if (!role || !email || !password) {
-            toast.error("Please Provide All Fields");
+            return toast("Please Pride All Fields");
         }
-        store.dispatch(userLogin({ email, password }));
+        await store.dispatch(userLogin({ email, password, role }));
     } catch (error) {
         console.log(error);
     }
@@ -32,23 +107,6 @@ export const handleRegister = async (
         if (!role || !email || !password || !phone || !address || !website || (!name && !organisationName && !hospitalName)) {
             toast.error("Please Provide All Fields");
         }
-
-        await store.dispatch(getCurrentUser());
-
-        const state = store.getState();
-        const users = state.auth.user;
-
-        if (users === null) {
-            toast.error("User data is still loading. Please try again.");
-            return;
-        }
-
-        const existingUser = users.find((user) => user.email === email);
-        if (existingUser) {
-            toast.error("This email is already registered");
-            return;
-        }
-
         await store.dispatch(
             userRegister({
                 name,
@@ -62,10 +120,8 @@ export const handleRegister = async (
                 website,
             })
         );
-
         toast.success("Registration successful!");
-
-        const navigate = useNavigate();
+       const navigate = useNavigate();
         navigate("/login");
     } catch (error) {
         console.log(error);
