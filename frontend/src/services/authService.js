ebd0232764211661f849249d5670f04176e278e7
Omerta85 <666Omerta@gmail.com>
2023-08-11 19:13:12 +0300

    fixed frontend

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -14,7 +14,41 @@ export const handleLogin = (e, email, password, role) => {
     }
 };
 
-export const handleRegister = (
+// export const handleRegister = (
+//     e,
+//     name,
+//     role,
+//     email,
+//     password,
+//     phone,
+//     organisationName,
+//     address,
+//     hospitalName,
+//     website
+// ) => {
+//     e.preventDefault();
+//     try {
+//         if (!role || !email || !password || !phone || !address ||!website || (!name && !organisationName && !hospitalName)) {
+//             return toast("Please Pride All Fields");
+//         }
+//         store.dispatch(
+//             userRegister({
+//                 name,
+//                 role,
+//                 email,
+//                 password,
+//                 phone,
+//                 organisationName,
+//                 address,
+//                 hospitalName,
+//                 website,
+//             })
+//         );
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+export const handleRegister = async (
     e,
     name,
     role,
@@ -28,10 +62,20 @@ export const handleRegister = (
 ) => {
     e.preventDefault();
     try {
-        if (!role || !email || !password || !phone || !address ||!website || (!name && !organisationName && !hospitalName)) {
-            return toast("Please Pride All Fields");
+        if (!role || !email || !password || !phone || !address || !website || (!name && !organisationName && !hospitalName)) {
+            return toast("Please Provide All Fields");
         }
-        store.dispatch(
+
+        // Check if the email is already registered
+        const state = store.getState(); // Get the current state from the Redux store
+        const users = state.auth.users; // Assuming you have a "users" property in your Redux state
+
+        const existingUser = users.find((user) => user.email === email);
+        if (existingUser) {
+            return toast("This email is already registered");
+        }
+
+        await store.dispatch(
             userRegister({
                 name,
                 role,
@@ -44,6 +88,8 @@ export const handleRegister = (
                 website,
             })
         );
+
+        toast("Registration successful!");
     } catch (error) {
         console.log(error);
     }
